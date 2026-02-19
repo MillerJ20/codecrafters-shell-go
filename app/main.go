3c0fd0d212c561bc3d1cc59046e3d594b8828c5f
Miller Johnston <miller.johnston17@gmail.com>
2026-02-19 00:47:16 -0800

    Remove commented Scanln

--- app/main.go
+++ app/main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"fmt";
+	"log"
 )
 
 func main() {
 	fmt.Print("$ ")
 
 	var command string
-	/*
+	
 	n, err := fmt.Scanln(&command)
 	if err != nil {
 		log.Fatal(err)
 	}
-	*/
+	
+	if n < 0 {
+		log.Fatal("Wow bad number!")
+	}
 
 	fmt.Printf("%s: command not found", command)
 }
