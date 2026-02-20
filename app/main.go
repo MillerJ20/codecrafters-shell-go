48c9c9421aa2d1f01d675745b3ea16019b04bebb
Miller Johnston <miller.johnston17@gmail.com>
2026-02-19 22:45:29 -0800

    Remove whitespace

--- app/main.go
+++ app/main.go
@@ -29,7 +29,7 @@ func parseCommand(line string) {
 		os.Exit(0)
 	case "echo":
 		if len(parts) > 1{
-			fmt.Printf(" %s \n", parts[1])
+			fmt.Printf("%s \n", parts[1])
 		}
 	default:
 		fmt.Printf("%s: command not found \n", command)
